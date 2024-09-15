b587a690ca5d97fc350f06094f5d7c4bb5fde96e
Mvoi <mvoifranklin@gmail.com>
2024-09-15 16:10:12 +0300

    Create textures_fn.c - functions to loadthe textures, render and destroy

---
+++ src/textures_fn.c
@@ -0,0 +1,39 @@
+#include "textures.h"
+#include "maze.h"
+
+// TODO: documet the functions
+
+SDL_Texture *hedge_texture = NULL;
+SDL_Texture *tree_texture = NULL;
+
+void load_textures(SDL_Renderer *renderer)
+{
+  hedge_texture = IMG_LoadTexture(renderer, "hedge.pgn");
+
+  if (!hedge_texture)
+  {
+    printf("Error loading hedge texture: %s\n", SDL_GetError());
+    exit(1);
+  }
+
+  tree_texture = IMG_LoadTexture(renderer, "tree.png");
+
+  if (!tree_texture)
+  {
+    printf("Error loading tree texture: %s\n", SDL_GetError());
+    exit(1);
+  }
+}
+
+void free_textures()
+{
+  if (hedge_texture)
+  {
+    SDL_DestroyTexture(hedge_texture);
+  }
+
+  if (tree_texture)
+  {
+    SDL_DestroyTexture(tree_texture);
+  }
+}
